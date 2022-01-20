4b72cc14715140c930b23ce715dbb035ac4b5884
Dubrov Ruslan <dubrov.ruslan@gmail.com>
2022-01-21 00:39:14 +0200

    Done Module4HW3

---
+++ Module4HW3/EntitiyConfigurations/ProjectConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Module4HW3.Entities;
+
+namespace Module4HW3.EntitiyConfigurations
+{
+    public class ProjectConfiguration : IEntityTypeConfiguration<Project>
+    {
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+            builder.ToTable("Project").HasKey(p => p.ProjectId);
+            builder.Property(p => p.ProjectId).HasColumnType("int").IsRequired().ValueGeneratedOnAdd();
+            builder.Property(p => p.Name).HasColumnType("nvarchar").IsRequired().HasMaxLength(50);
+            builder.Property(p => p.Budget).HasColumnType("money").IsRequired();
+            builder.Property(p => p.StartedDate).HasColumnType("datetime2").IsRequired().HasMaxLength(7);
+        }
+    }
+}
--- Module4HW3/Program.cs
+++ Module4HW3/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Module4HW3
 {
@@ -6,7 +9,19 @@ namespace Module4HW3
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("Config.json");
+            var config = builder.Build();
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            var optionBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+            var options = optionBuilder.UseSqlServer(connectionString).Options;
+            using (var db = new ApplicationContext(options))
+            {
+                db.SaveChanges();
+            }
+
+            Console.Read();
         }
     }
 }

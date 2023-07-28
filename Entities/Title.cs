ddca0f570100598e790544aded38c161c1df4250
DubrovRuslan <dubrov.ruslan@gmail.com>
2023-07-28 12:45:38 +0300

    Entities done

---
+++ Entities/Title.cs
@@ -0,0 +1,9 @@
+﻿namespace Module4HW3.Entities
+{
+    public class Title
+    {
+        public int TitleId { get; set; }
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+}
